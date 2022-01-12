a6652d7c6ec40bbcea4e80c075705a53dbe34e86
ruymon <ruystfrancis@gmail.com>
2022-01-12 00:16:55 -0300

    feature resize container

--- README.md
+++ README.md
@@ -26,3 +26,20 @@ I have made a simple HTML file with a wrapper div and a JS file injecting child 
 Currently I am just adding planes but not actualy animating them... The animation is quite simple just a horizontal displacement. Pro tip: The side the plane is going will change the direction of the animation.
 
 At this time this I open this place for us to discuss the best way to implement this.
+
+---
+
+
+Todo: add config object for lib.
+
+Idea:
+```js
+
+const config = {
+    maxLimit: 'container' | number, // Limit of Spawnable airplanes
+    background: 'transparent' | 'hexColor', // Container background color
+    resizable: true | false, // Resizable container
+    colorFromLeft: [array of hexColor] | 'hexColor' | 'random', // Color of the airplanes coming from the left side. 
+    colorFromRight: [array of hexColor] | 'hexColor', // Color of the airplanes coming from the right side. 
+}
+```
--- airway.js
+++ airway.js
@@ -1,42 +1,68 @@
-const container = document.getElementById('airwayGrid');
+const wrapper = document.getElementById('airwayGrid');
 
-function generateAirplane() {
-    const randomClass = Math.random() < 0.5 ? 'airplaneRight' : 'airplaneLeft';
-
-    const randomPlane = `
-        <div class="${randomClass} airplaneContent">
-            <div class="airplaneLine"></div>
-            <svg width="34" height="32" viewBox="0 0 34 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg" >
-                <path d="M23.579 -8.39233e-05H20.2106L11.7895 13.4736H2.52632C1.12842 13.4736 0 14.6021 0 16C0 17.3979 1.12842 18.5263 2.52632 18.5263H11.7895L20.2106 32H23.579L19.3685 18.5263H28.6317L31.158 21.8947H33.6843L32.0001 16L33.6843 10.1052H31.158L28.6317 13.4736H19.3685L23.579 -8.39233e-05Z" />
-            </svg>
-        </div>
-    `;
-    container.insertAdjacentHTML('beforeend', randomPlane);
-};
-
-function getRandomSeconds(min, max) {   
-    min = Math.ceil(min);   
-    max = Math.floor(max);   
-    return Math.floor((Math.random() * (max - min) + min)) * 1000;
-};
-
-let myInterval = setInterval(updateTimeout, 1000); // First call to updateTimeout
-const containerHeightLimit = container.offsetHeight;
-
-function updateTimeout() {
-    const time = getRandomSeconds(3, 6);
-    const airplaneCount = container.querySelectorAll('.airplaneContent').length;
-    const airplaneCountLimit = Math.floor(containerHeightLimit / 32) - 1;
-
-    console.warn(`INFO: This container fits: ${airplaneCountLimit + 1} airplane(s) and currently we have: ${airplaneCount + 1} airplane(s) flying!`);
-
-    generateAirplane();
-    clearInterval(myInterval);
-
-    if (airplaneCount < airplaneCountLimit) {
-        myInterval = setInterval(updateTimeout, time);
-    } else {
-        console.log('-----------------');
-        console.warn(`INFO: Airplane limit reached!`);
+function executeAirplanes(container) {
+    function generateAirplane() {
+        const randomClass = Math.random() < 0.5 ? 'airplaneRight' : 'airplaneLeft';
+    
+        const randomPlane = `
+            <div class="${randomClass} airplaneContent">
+                <div class="airplaneLine"></div>
+                <svg width="34" height="32" viewBox="0 0 34 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg" >
+                    <path d="M23.579 -8.39233e-05H20.2106L11.7895 13.4736H2.52632C1.12842 13.4736 0 14.6021 0 16C0 17.3979 1.12842 18.5263 2.52632 18.5263H11.7895L20.2106 32H23.579L19.3685 18.5263H28.6317L31.158 21.8947H33.6843L32.0001 16L33.6843 10.1052H31.158L28.6317 13.4736H19.3685L23.579 -8.39233e-05Z" />
+                </svg>
+            </div>
+        `;
+        container.insertAdjacentHTML('beforeend', randomPlane);
     };
-};
+
+    function clearAirplanes(maxLimit) {
+        const airplanesFlying = container.querySelectorAll('.airplaneContent');
+
+        airplanesFlying.forEach((airplane, index) => {
+            if (index > maxLimit - 1) {
+                airplane.remove();
+                console.log('Airplane removed');
+            }
+        });
+    };
+    
+    function getRandomSeconds(min, max) {   
+        min = Math.ceil(min);   
+        max = Math.floor(max);   
+        return Math.floor((Math.random() * (max - min) + min)) * 1000;
+    };
+    
+    let myInterval = setInterval(updateTimeout, 0); // First call to updateTimeout
+    
+    function updateTimeout() {
+        const time = getRandomSeconds(3, 6);
+        const airplaneCount = container.querySelectorAll('.airplaneContent').length;
+
+        const containerHeightLimit = container.offsetHeight;
+        const airplaneCountLimit = Math.floor(containerHeightLimit / 32) - 1;
+    
+        console.warn(`INFO: This container fits: ${airplaneCountLimit + 1} airplane(s) and currently we have: ${airplaneCount + 1} airplane(s) flying!`);
+
+        if (airplaneCount >= airplaneCountLimit) {
+            clearAirplanes(airplaneCountLimit);
+        }
+    
+        generateAirplane();
+        clearInterval(myInterval);
+    
+        if (airplaneCount < airplaneCountLimit) myInterval = setInterval(updateTimeout, time);
+        else console.error(`INFO: Airplane limit reached!`);
+    };
+
+    const containerHeight = container.offsetHeight;
+
+    new ResizeObserver(() => {
+        const newContainerHeight = container.offsetHeight;
+        if(containerHeight === newContainerHeight) return;
+
+        console.log(`Container has been resized to: ${newContainerHeight}`);
+        updateTimeout();
+        
+    }).observe(container);
+
+}; executeAirplanes(wrapper);
